f79079651a26a0dac11b7517dfa3ff28bc86a292
Ray Donnelly <mingw.android@gmail.com>
2016-10-29 17:16:07 +0100

    Win32 (MSVC): Define {UN,}LIKELY

--- src/cpp/core/include/core/Macros.hpp
+++ src/cpp/core/include/core/Macros.hpp
@@ -37,6 +37,9 @@
 #if defined(__GNUC__)
 # define LIKELY(x)   __builtin_expect(!!(x), 1)
 # define UNLIKELY(x) __builtin_expect(!!(x), 0)
+#elif defined(_MSC_VER)
+# define LIKELY(x)   (!!(x))
+# define UNLIKELY(x) (!!(x))
 #else
 # define LIKELY(x)   (x)
 # define UNLIKELY(x) (x)
